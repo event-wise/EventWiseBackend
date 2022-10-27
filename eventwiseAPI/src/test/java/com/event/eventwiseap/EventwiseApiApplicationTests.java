d2d1f75290313da1d061672bd70728665636de98
balik18 <55997609+YigitBalik@users.noreply.github.com>
2022-10-27 15:49:38 +0300

    Project initialization

---
+++ eventwiseAPI/src/test/java/com/event/eventwiseap/EventwiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.eventwiseap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventwiseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
