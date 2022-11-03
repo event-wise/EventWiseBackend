5a5f0ce8073f594b03565a8d20a7b448a070d3bb
balik18 <55997609+YigitBalik@users.noreply.github.com>
2022-11-03 21:29:58 +0300

    Entity Implementation: BaseEntity, RoleType, Role and User entities.

---
+++ eventwiseAPI/src/main/java/com/event/eventwiseap/model/RoleType.java
@@ -0,0 +1,5 @@
+package com.event.eventwiseap.model;
+
+public enum RoleType {
+    ROLE_USER, ROLE_ADMIN
+}
--- eventwiseAPI/src/test/java/com/event/eventwiseap/EventwiseApiApplicationTests.java
+++ eventwiseAPI/src/test/java/com/event/eventwiseap/EventwiseApiApplicationTests.java
@@ -1,13 +1,33 @@
 package com.event.eventwiseap;
 
+import com.event.eventwiseap.model.Role;
+import com.event.eventwiseap.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootTest
 class EventwiseApiApplicationTests {
 
     @Test
     void contextLoads() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        Set<Integer> roles = new HashSet<>();
+        roles.add(1);
+        roles.add(2);
+//        User user = User.builder()
+//                        .username('balik')
+//                        .email('balik18@itu.edu.tr')
+//                        .displayedName('balon')
+//                        .password(encoder.encode(('bal_admin')))
+//                        .location('Istanbul')
+//                        .roles(roles)
+//                        .build();
+
     }
 
 }
