73d04e3e6cbc02adad23d4f9b4a91f2ffc92b4ec
ituitis-kamburoglu19 <tumaykamburoglu@gmail.com>
2022-11-09 23:11:31 +0300

    AWS db connection was configured

---
+++ eventwiseAPI/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into role(name) values ('ROLE_ADMIN');
+insert into role(name) values ('ROLE_USER');
+-- insert into "user"(email,username,password,role_id)values ('balik18@itu.edu.tr', 'balik', '$2a$10$J7jbVrC8yKs3y9y/M.ZP/O3VAEuORb3W/fzYtIRU9IE9q6bQvzloq', 1);
